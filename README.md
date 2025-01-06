c86672ce9bd3f7fa406ad216eb87666277752685
当时只道是寻常 <1207588603@qq.com>
2025-01-06 21:19:25 +0800

    perf: 导航效果

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+## 阅读指南
+
+- 具体的 HTML + CSS 小案例都放在 demo 目录
+- 根目录的 index.html 只是为了使用 GitHub 的静态部署
+- [在线预览](https://ace627.github.io/html-template)
+
 ## 常用网站
 
 - [ChatGPT](https://chatgpt.com)
--- scripts/auto-nav-db.mjs
+++ scripts/auto-nav-db.mjs
@@ -3,6 +3,8 @@ import fs from 'fs/promises'
 
 async function bootstrap() {
   const dirNameList = await fs.readdir(path.join(process.cwd(), 'demo'))
+  const savePath = path.join(process.cwd(), './database/navs.json')
+  await fs.writeFile(savePath, JSON.stringify(dirNameList))
 }
 
 bootstrap()
